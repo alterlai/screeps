72f9ccf5875340ebb32c0ab919e407bf1b4005fe
alterlai <jeroen.jeroenvdlaan@gmail.com>
2016-11-29 18:51:17 +0100

    comment fixes

--- logic.spawning.js
+++ logic.spawning.js
@@ -11,12 +11,12 @@ roleSpawning = {
             console.log('Spawning new harvester: ' + newName);
         }
 
-        // Spawn Upgraders if there are less than 2 upgraders.
+        // Spawn Upgraders if there are less than 4 upgraders.
     	if(totalUpgraders.length < 4 && spawn.energy > 250) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader', resourceRoomIndex: 0});
             console.log('Spawning new upgrader: ' + newName);
         }
-        // Spawn Upgraders if there are less than 2 upgraders.
+        // Spawn Upgraders if there are less than 2 builders.
         if(totalBuilders.length < 2 && spawn.energy > 250) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'builder', resourceRoomIndex: 0});
             console.log('Spawning new builder: ' + newName);
