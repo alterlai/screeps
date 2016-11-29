d44996ed6ef71db1ecfdc24f04790a7f95f80953
Jeroen van der Laan <Jeroen.jeroenvdlaan@gmail.com>
2016-11-29 19:12:14 +0100

    More creeps

--- logic.spawning.js
+++ logic.spawning.js
@@ -5,20 +5,24 @@ roleSpawning = {
     	var totalUpgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         var totalBuilders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
+        var ammountOfHarvesters = 2;
+        var ammountOfUpgraders = 4;
+        var ammountOfBuilders = 3;
+
     	// Spawn harvester if there are less than 2 harvesters.
-    	if(totalHarvesters.length < 2 && spawn.energy > 250) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester', resourceRoomIndex: 0});
+    	if(totalHarvesters.length < ammountOfHarvesters && spawn.energy > 250) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'harvester', resourceRoomIndex: 0});
             console.log('Spawning new harvester: ' + newName);
         }
 
         // Spawn Upgraders if there are less than 2 upgraders.
-    	if(totalUpgraders.length < 4 && spawn.energy > 250) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader', resourceRoomIndex: 0});
+    	if(totalUpgraders.length < ammountOfUpgraders && spawn.energy > 250) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'upgrader', resourceRoomIndex: 0});
             console.log('Spawning new upgrader: ' + newName);
         }
         // Spawn Upgraders if there are less than 2 upgraders.
-        if(totalBuilders.length < 2 && spawn.energy > 250) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'builder', resourceRoomIndex: 0});
+        if(totalBuilders.length < ammountOfBuilders && spawn.energy > 250) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'builder', resourceRoomIndex: 0});
             console.log('Spawning new builder: ' + newName);
         }
     }
