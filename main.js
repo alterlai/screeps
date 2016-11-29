9f68b78f73b09444d15c7f2793846bdbac91f9d1
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2016-11-29 13:16:46 +0100

    added roles harvester, builder, upgrader

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
@@ -25,6 +26,10 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder')
+        {
+            roleBuilder.run(creep);
         }
     }
 }
